7ec39172fe4852b1a538ecec9472db0797855ceb
Karan Parashar <whyredfire@gmail.com>
2022-08-27 13:54:24 +0530

    vendor: Import pixel_2016_exclusive XML for Google Photos
    Change-Id: I3cdb84f3e8d148b2619b5490bf18b744c5b417d4

--- config/flare_vault.mk
+++ config/flare_vault.mk
@@ -18,3 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Include Pixel GMS
 $(call inherit-product, vendor/flare/config/pixel.mk)
+
+# Google Photos Pixel Exclusive XML
+PRODUCT_COPY_FILES += \
+    vendor/flare/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
