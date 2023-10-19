67a2406c5b4c39d0debe5a45ce923aed0c3dc0f5
hoffc <quic_hoffc@quicinc.com>
2023-10-19 17:19:22 +0800

    config: qssi: Disable initial status to stopped for some AOSP packages
    Disable the initial package status to stopped for some AOSP packages, or not, it will result in CTS test case failure after enabling new feature stopSystemPackagesByDefault.
    
    Change-Id: I7c1becc45c0975606840acabec31f4b4f5fdb9f3
    CRs-Fixed: 3603517
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- config/flare_packages.mk
+++ config/flare_packages.mk
@@ -3,6 +3,10 @@ PRODUCT_PACKAGES += \
     custom_charger_animation \
     custom_charger_animation_vendor
 
+# Enable allowlist for some aosp packages that should not be scanned in a "stopped" state
+# Some CTS test case failed after enabling feature config_stopSystemPackagesByDefault
+PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
+
 # Overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/flare/overlay/common_pixel
