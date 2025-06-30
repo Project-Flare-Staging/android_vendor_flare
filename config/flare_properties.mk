0d8b9eb0bc73b37c6e8e7aff8c0933e5378983b5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    config: Add custom blur support
    Change-Id: Ie503986ef2642dbd474bfa5bee8d67688788d544

--- config/flare_properties.mk
+++ config/flare_properties.mk
@@ -1,3 +1,15 @@
+# Custom Blur Ui
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
+endif
+
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
