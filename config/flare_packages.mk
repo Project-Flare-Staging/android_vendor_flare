9cb7a31146bb1a1bb8424760979c485e3502fa28
hridaya <hridaya@pixelos.net>
2025-10-17 22:54:21 +0545

    vendor: Import device config overrides
    * Move existing RRO to rro_packages.
    
    Change-Id: I09bf3998eb096f2bb140e297ce8c7fc53eb72d5b

--- config/flare_packages.mk
+++ config/flare_packages.mk
@@ -4,6 +4,9 @@ PRODUCT_PACKAGES += \
     custom_charger_animation_vendor
 
 # Overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/flare/overlay/common_pixel
+
 PRODUCT_PACKAGES += \
     FrameworkOverlayCustom \
     SettingsOverlayCustom
