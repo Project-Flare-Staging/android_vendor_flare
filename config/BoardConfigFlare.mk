ad2ba134e87769be6e9ba1954056f8f6d10fb763
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-15 20:18:23 +0530

    config: Include device/qcom/common
    Change-Id: Ia1e5349bbc2e903a1fe382c94670f2b356662e6e

--- config/BoardConfigFlare.mk
+++ config/BoardConfigFlare.mk
@@ -10,4 +10,9 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
+ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
+# Include our Qualcomm Board configuration.
+include device/qcom/common/BoardConfigQcom.mk
+endif
+
 include vendor/flare/config/BoardConfigSoong.mk
