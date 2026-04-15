09d9eb4365d7f7c2b75ac760e1b782bf0ebb4766
ProjectFlare-OSS <projectflare2000@gmail.com>
2026-04-15 19:39:17 +0530

    config: Import flare configs
    * We will store our additions here.
    
    Change-Id: Idb629f5953bb345cf36ae7ceeafa51a573edaf23

---
+++ config/flare_vault.mk
@@ -0,0 +1,5 @@
+# Inherit flare packages
+$(call inherit-product, vendor/flare/config/flare_packages.mk)
+
+# Inherit flare properties
+$(call inherit-product, vendor/flare/config/flare_properties.mk)
