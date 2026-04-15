3f2d0bd6cd54136133d564f2fa86f6e6f1640c63
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-15 20:19:22 +0530

    config: Inherit SD Clang
    Change-Id: If03e82d9616860cb3b7c9a062d01a2902101d2cf

--- config/flare_vault.mk
+++ config/flare_vault.mk
@@ -6,3 +6,6 @@ $(call inherit-product, vendor/flare/config/flare_properties.mk)
 
 # Qualcomm Common
 $(call inherit-product, device/qcom/common/common.mk)
+
+# Snapdragon Clang
+$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
