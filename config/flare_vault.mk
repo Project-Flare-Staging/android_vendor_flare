b4ac4bec8b1bfdf0f502a9a494e8f10ba5b6ac9f
ProjectFlare-OSS <projectflare2000@gmail.com>
2026-04-15 20:18:23 +0530

    config: Include device/qcom/common
    Change-Id: Ia1e5349bbc2e903a1fe382c94670f2b356662e6e

--- config/flare_vault.mk
+++ config/flare_vault.mk
@@ -3,3 +3,6 @@ $(call inherit-product, vendor/flare/config/flare_packages.mk)
 
 # Inherit flare properties
 $(call inherit-product, vendor/flare/config/flare_properties.mk)
+
+# Qualcomm Common
+$(call inherit-product, device/qcom/common/common.mk)
