6aff2aff18392938f000b3f38073182d67b838d2
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-16 18:31:36 +0530

    overlay: Move custom overlays to pixel
    Change-Id: I91f8bc3f9aa09bf700087a8c612e3241cd5a05ba

--- config/flare_packages.mk
+++ config/flare_packages.mk
@@ -18,8 +18,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/flare/overlay/common_pixel
 
 PRODUCT_PACKAGES += \
-    FrameworkOverlayCustom \
-    SettingsOverlayCustom
+    FrameworkOverlayPixel \
+    SettingsOverlayPixel
 
 # Protobuf - Workaround for prebuilt Qualcomm HAL
 PRODUCT_PACKAGES += \
