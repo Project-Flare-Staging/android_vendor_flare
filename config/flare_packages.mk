27222fb3452c651a063ccd201d16c496ba0d7698
Vishalcj17 <vishalcj@aospa.co>
2026-04-21 15:38:55 +0530

    config: Build HIDL base and manager
    Change-Id: I4775c26858f29f9f7f7ef5a618b36ef67048f5ea

--- config/flare_packages.mk
+++ config/flare_packages.mk
@@ -1,6 +1,13 @@
 # Enable some aosp packages allowlist
 PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
 # Telephony - CLO
 PRODUCT_PACKAGES += \
     extphonelib \
