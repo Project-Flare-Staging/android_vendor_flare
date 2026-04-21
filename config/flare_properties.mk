dca14aa79ac47a02c6275caa8fc39c32dc80d865
xboxfanj <jake@aospa.co>
2026-04-21 15:59:34 +0530

    config: Move multifile cache property to system_ext
    This is consistent with our other properties and fixes denials.
    
    Change-Id: I4c9973c4c1c491c329f2424e241a791d414b61a3

--- config/flare_properties.mk
+++ config/flare_properties.mk
@@ -7,6 +7,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
 
 # EGL - Blobcache configuration
-PRODUCT_VENDOR_PROPERTIES += \
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.egl.blobcache.multifile=true \
     ro.egl.blobcache.multifile_limit=33554432
