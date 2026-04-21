724beaefb47b37f425dc8a85adead63a1b4baf03
Vishalcj17 <vishalcj@aospa.co>
2026-04-21 15:38:55 +0530

    config: Build HIDL base and manager
    Change-Id: I4775c26858f29f9f7f7ef5a618b36ef67048f5ea

--- config/flare_packages.mk
+++ config/flare_packages.mk
@@ -6,6 +6,13 @@ PRODUCT_PACKAGES += \
 # Enable some aosp packages allowlist
 PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
 # Overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/flare/overlay/common_pixel
