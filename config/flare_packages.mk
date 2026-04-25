63eab9b21fbfdd99216d82e0646136c445f2e7a6
Jake Weinstein <jake@aospa.co>
2026-04-25 13:39:15 +0530

    config: Build QTI telephony packages
    * These are needed by all kind of devices, without this, device won't boot.
    
    Co-authored-by: Tobias Merkel <stylogey@gmail.com>
    Change-Id: I2759d4634a0aacd2f96280a0d305202e3208e14a

---
+++ config/flare_packages.mk
@@ -0,0 +1,26 @@
+# Telephony - CLO
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml
+
+ifneq ($(TARGET_NO_TELEPHONY), true)
+PRODUCT_PACKAGES += \
+    tcmiface \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml
+
+PRODUCT_BOOT_JARS += \
+    tcmiface \
+    telephony-ext
+endif
