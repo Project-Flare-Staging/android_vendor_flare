e1215e330f68118e29ca047f66ba90b73181810e
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2026-05-03 22:54:44 +0530

    config: Remove duplicate declaration
    * We already have these in vendor/addons
    * Also fix a typo
    
    Change-Id: Ia3b44f8d7b2ce1ab584c6aac55b131c917002204

--- config/pixel.mk
+++ config/pixel.mk
@@ -1,8 +1,5 @@
 WITH_GMS := true
 
-# Pixel Clocks
-$(call inherit-product, vendor/pixel/clocks/products/clocks.mk)
-
 # Pixel GMS
 $(call inherit-product, vendor/pixel/gms/products/gms.mk)
 
@@ -12,7 +9,7 @@ $(call inherit-product, vendor/pixel/gsans/products/gsans.mk)
 # Pixel Launcher
 $(call inherit-product, vendor/pixel/launcher/products/launcher.mk)
 
-# Pixel ThemePicker
+# Pixel Sounds
 $(call inherit-product, vendor/pixel/sounds/products/sounds.mk)
 
 # Pixel ThemePicker
